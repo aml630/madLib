b506b079c44b289026707e93c43f07df7da1e011
Tim Alex <Tim Alex>
2016-02-08 14:32:38 -0800

    add files?

---
+++ Objects/madlibObject.cs
@@ -0,0 +1,11 @@
+namespace madLibLibrary {
+
+  public class storyVariables {
+    public string name1 {get; set; }
+    public string name2 {get; set; }
+    public string creature {get; set; }
+    public string weapon {get; set; }
+    public string vehicle {get; set; }
+    public string treasure {get; set; }
+  }
+}
